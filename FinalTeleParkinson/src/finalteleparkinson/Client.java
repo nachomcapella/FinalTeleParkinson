825ab25018712e5464e4d7d4c2fc395c5eb73c9b
nachomcapella <nachetemc97@gmail.com>
2018-12-02 16:59:15 +0100

    We can now save bitalino signals.

--- FinalTeleParkinson/src/finalteleparkinson/Client.java
+++ FinalTeleParkinson/src/finalteleparkinson/Client.java
@@ -22,33 +22,35 @@ public class Client {
         printWriter.println(clin_history);
 
         //After sending the clinic history time to record em signals.
-        System.out.println("Signal recording begins. At least 2 have to be recorded");
+        System.out.println("Signal recording begins. At least 2 have to be recorded.");
+        System.out.println("Signal recording may take some time to start. Please, be patient.");
+
         //String signal_example = "2\n2\n-4\n9\nx"; //LAS SEÑALES  devueltas por el metofo que quedan tienen en la primera linea lel tipo de dat, en la última una x y en tre medias los valores grabados
         //System.out.println(signal_example);
-
         Scanner scanner = new Scanner(System.in);
         for (int i = 0;; i++) {
-            System.out.println("Which type of signal would you like to record?");
-            System.out.println("EMG (0)");
-            System.out.println("ECG (1)");
-            System.out.println("EDA (2)");
-            System.out.println("EEG (3)");
-            System.out.println("ACC (4)");
-            System.out.println("EMG (5)");
-            System.out.print("Option: ");
-            String line = scanner.nextLine();
-
-            int signalType = Integer.parseInt(line);
-            System.out.println("Option " + signalType + " chosen.");
-
             if (i < 2) {
+                System.out.println("Which type of signal would you like to record?");
+                System.out.println("EMG (0)");
+                System.out.println("ECG (1)");
+                System.out.println("EDA (2)");
+                System.out.println("EEG (3)");
+                System.out.println("ACC (4)");
+                System.out.println("LUX (5)");
+                System.out.print("Option: ");
+                String line = scanner.nextLine();
+
+                int signalType = Integer.parseInt(line);
+                System.out.println("Option " + signalType + " chosen.");
                 System.out.println("Record of signal " + (i + 1) + " begins");
                 //String biosignal = IO.obtainBioSignal();
                 //printWriter.println(signal_example);
                 Frame[] frame = IO.readBitalinoSignal(signalType);
-                String signal = "";
-                for (int j = 0; j < frame.length; j++) {
-                    signal = signal + frame[j].analog[signalType] + "\n";
+                String signal = "2\n";
+                for (int k = 0; k < 10; k++) {
+                    for (Frame frame1 : frame) {
+                        signal = signal + frame1.analog[0] + "\n";
+                    }
                 }
                 printWriter.println(signal);
             }
@@ -60,10 +62,24 @@ public class Client {
                     //String biosignal = IO.obtainBioSignal();
                     //System.out.println("Record of " + (i+1) + "signal begins");
                     //printWriter.println(signal_example);
+                    System.out.println("Which type of signal would you like to record?");
+                    System.out.println("EMG (0)");
+                    System.out.println("ECG (1)");
+                    System.out.println("EDA (2)");
+                    System.out.println("EEG (3)");
+                    System.out.println("ACC (4)");
+                    System.out.println("LUX (5)");
+                    System.out.print("Option: ");
+                    String line = scanner.nextLine();
+
+                    int signalType = Integer.parseInt(line);
+                    System.out.println("Option " + signalType + " chosen.");
                     Frame[] frame = IO.readBitalinoSignal(signalType);
-                    String signal = "";
-                    for (int j = 0; j < frame.length; j++) {
-                        signal = signal + frame[j].analog[signalType] + "\n";
+                    String signal = "2\n";
+                    for (int k = 0; k < 10; k++) {
+                        for (Frame frame1 : frame) {
+                            signal = signal + frame1.analog[signalType] + "\n";
+                        }
                     }
                     printWriter.println(signal);
                 } else if (answer.equalsIgnoreCase("n")) {
@@ -75,5 +91,8 @@ public class Client {
                 }
             }
         }
+        System.out.println("Process finished. Proceeding to disconnect.");
+        System.out.println("Goodbye.");
     }
+
 }
